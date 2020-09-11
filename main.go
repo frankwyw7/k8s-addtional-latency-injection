0fd98e16426bae1940b6044abb636526e268ffcc
frankwwShadow <wangyaowei7@gmail.com>
2020-09-11 10:03:39 +0800

    add basic server

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"strings"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+var needInjectDeployments []v1.Deployment = make([]v1.Deployment, 7, 8)
 
 func main() {
 	r := gin.Default()
@@ -12,4 +20,43 @@ func main() {
 		})
 	})
 	r.Run()
+
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+	judgeError(err)
+
+	client, err := kubernetes.NewForConfig(config)
+	dList, derr := client.AppsV1().Deployments("namespace").List(
+		nil,
+		metav1.ListOptions{},
+	)
+	judgeError(derr)
+
+	for _, deployment := range dList.Items {
+		if(filterDeployment(deployment)) {
+			_ = append(needInjectDeployments, deployment)
+		}
+	}
+
+	//spec := dList.Items[0].Spec
+	//specStr := spec.String()
+	//spec.Template.
+	//
+	//for _, deploy := range dList.Items {
+	//
+	//}
+
+}
+
+//func updateK8s(old :v1.oldDeployment, new :v1) {
+//
+//}
+
+func filterDeployment(deployment v1.Deployment) bool {
+	return strings.Contains(strings.ToLower(deployment.Name), "tikv")
+}
+
+func judgeError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }

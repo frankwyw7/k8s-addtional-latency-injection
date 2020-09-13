8c6060b6f3d1935e618905b91e149f30d2b68eeb
frankwwShadow <wangyaowei7@gmail.com>
2020-09-13 14:09:30 +0800

    add basic api that communicate with k8s

--- main.go
+++ main.go
@@ -1,42 +1,73 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"strings"
-	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 var needInjectDeployments []v1.Deployment = make([]v1.Deployment, 7, 8)
 
 func main() {
+	println("go start")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	r.GET("/getPodList", printPodList)
 
-	config, err := clientcmd.BuildConfigFromFlags("", "")
-	judgeError(err)
+	println("go listen start")
 
-	client, err := kubernetes.NewForConfig(config)
-	dList, derr := client.AppsV1().Deployments("namespace").List(
-		nil,
-		metav1.ListOptions{},
-	)
-	judgeError(derr)
+	r.Run(":8092")
 
-	for _, deployment := range dList.Items {
-		if(filterDeployment(deployment)) {
-			_ = append(needInjectDeployments, deployment)
-		}
-	}
 
+	//
+	//kubeconfigf := flag.String("kubeconfig", "/Users/mhausenblas/.kube/config", "absolute path to the kubeconfig file")
+	//flag.Parse()
+	//
+	//kubeconfig := filepath.Join(
+	//	os.Getenv("HOME"), ".kube", "config",
+	//)
+	//clientcmd.BuildConfigFromFlags()
+	//
+	//config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	//judgeError(err)
+	//
+	//clientset, err := kubernetes.NewForConfig(config)
+
+
+
+	//clientset.CoreV1().ConfigMaps().List()
+	//dList, dErr := clientset.AppsV1().Deployments("namespace").List(
+	//	nil,
+	//	metav1.ListOptions{},
+	//)
+	//judgeError(dErr)
+	//
+	//println(dList)
+
+
+
+	//for _, deployment := range dList.Items {
+	//	if filterDeployment(deployment) {
+	//		needInjectDeployments = append(needInjectDeployments, deployment)
+	//	}
+	//}
+	//
+	//response := v1beta1.AdmissionResponse{Allowed: true}
+	//println(response)
+
+	//v1beta1.AdmissionResponse{Allowed: true, Patch: "aaa", PatchType: func() {}}
+	//v1beta1.PatchTypeJSONPatch
+	//v1beta
 	//spec := dList.Items[0].Spec
 	//specStr := spec.String()
 	//spec.Template.
@@ -50,6 +81,25 @@ func main() {
 //func updateK8s(old :v1.oldDeployment, new :v1) {
 //
 //}
+
+func printPodList(c *gin.Context) {
+	kubeconfig := flag.String("kubeconfig", "/root/.kube/config", "absolute path to the kubeconfig file")
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	judgeError(err)
+
+	clientset, err := kubernetes.NewForConfig(config)
+	judgeError(err)
+
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	judgeError(err)
+
+	for _, pod := range pods.Items {
+		fmt.Printf("%s %s\n", pod.GetName(), pod.GetCreationTimestamp())
+	}
+}
+
 
 func filterDeployment(deployment v1.Deployment) bool {
 	return strings.Contains(strings.ToLower(deployment.Name), "tikv")
